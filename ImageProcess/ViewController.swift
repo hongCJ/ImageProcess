c23f2d05b2f98daf724d9230cbacdba3fac04064
zhenghong <zhenghong@asiainfo.com>
2020-07-01 17:13:31 +0800

    Initial Commit

---
+++ ImageProcess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageProcess
+//
+//  Created by 郑红 on 2020/7/1.
+//  Copyright © 2020 com.zhenghong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ImageProcessTests/ImageProcessTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImageProcessTests.swift
+//  ImageProcessTests
+//
+//  Created by 郑红 on 2020/7/1.
+//  Copyright © 2020 com.zhenghong. All rights reserved.
+//
+
+import XCTest
+@testable import ImageProcess
+
+class ImageProcessTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
